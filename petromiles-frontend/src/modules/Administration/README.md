372fea65761d69b225c844b707844f488dc9c338
Gustavo Sanchez <40648663+gustavosr98@users.noreply.github.com>
2020-05-04 09:20:17 -0400

    FASE1-001 Establecer estructura inicial de frontend (#2)
    * FASE1-001: Definir estructura de proyecto de Vue
    - Estructura base del frontend a partir de Vue CLI 3
    - Inlcuye soporte para PWA
    - Inlcuye Vuex
    - Inlcuye VueRouter
    - Inlcuye Vuetify
    - Configuración básica de Prettier
    
    * FASE1-001: Estructurar carpetas
    
    - Para mantener una carpeta por modulo de la aplicación

---
+++ petromiles-frontend/src/modules/Administration/README.md
@@ -0,0 +1,4 @@
+Administration
+
+- components/
+- views/
---
+++ petromiles-frontend/src/modules/Example/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <hello-world msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/modules/Example/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    "hello-world": HelloWorld,
+  },
+};
+</script>
