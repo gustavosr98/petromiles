3518c796217949caaa49dc1087f968390d292f24
Christian Neira <caneira.14@est.ucab.edu.ve>
2020-06-12 03:18:21 -0400

    Historia de Usuario FASE2-013 y nuevo archivo

---
+++ petromiles-backend/src/constants/mailsSubjectConst.ts
@@ -0,0 +1,23 @@
+export const mailsSubjets = {
+	welcome: 'Welcome To PetroMiles',
+	invoice: {
+		english: 'Your transaction is being processed',
+		spanish: 'Su transacción está siendo procesada',
+	},
+	upgrade_to_gold: {
+		english: 'Thank you for your loyalty!',
+		spanish: 'Gracias por su lealtad!',
+	},
+	bank_a_registration: {
+		english: 'Bank Account Registration',
+		spanish: 'Registro de Cuenta Bancaria',
+	},
+	bank_a_verified: {
+		english: 'Bank Account Verification',
+		spanish: 'Verificación de Cuenta Bancaria',
+	},
+	bank_a_deletion: {
+		english: 'Bank account deleted',
+		spanish: 'Se elimino la cuenta bancaria',
+	},
+};
