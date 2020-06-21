233c65fad84df5f44009bc06034c28a4dd0a31d5
Gustavo Sanchez <gustavosr98@gmail.com>
2020-06-21 14:19:43 -0400

    FASE2-022: Control de consumo de Sendgrid

--- petromiles-backend/src/modules/mails/mails.service.ts
+++ petromiles-backend/src/modules/mails/mails.service.ts
@@ -16,15 +16,21 @@ export class MailsService {
   ) {}
 
   async sendEmail(msg) {
-    const from = this.sendGridConfig.emailFrom;
-    try {
-      await this.sendGridClient.send({ ...msg, from });
-      this.logger.verbose(
-        `[${ApiModules.MAILS}] {${msg.to}} An email with the subject "${msg.subject}" has been sent`,
-      );
-    } catch (err) {
-      this.logger.error(
-        `[${ApiModules.MAILS}] {${msg.to}} Problem sending email. Reason: ${err.message}`,
+    if (process.env.SENDGRID_ON === 'true') {
+      const from = this.sendGridConfig.emailFrom;
+      try {
+        await this.sendGridClient.send({ ...msg, from });
+        this.logger.verbose(
+          `[${ApiModules.MAILS}] {${msg.to}} An email with the subject "${msg.subject}" has been sent`,
+        );
+      } catch (err) {
+        this.logger.error(
+          `[${ApiModules.MAILS}] {${msg.to}} Problem sending email. Reason: ${err.message}`,
+        );
+      }
+    } else {
+      this.logger.info(
+        `[${ApiModules.MAILS}] environment variable SENDGRID_ON is OFF`,
       );
     }
   }
