8b8158ea9adab3e6af926f3c9f6ea1abca7a5014
Gustavo Sanchez <gustavosr98@gmail.com>
2020-06-21 14:19:24 -0400

    FASE2-022: SSL configuration para BD en Heroku

--- petromiles-backend/src/database/database.service.ts
+++ petromiles-backend/src/database/database.service.ts
@@ -8,6 +8,12 @@ export const dataBaseProvider = TypeOrmModule.forRootAsync({
   async useFactory(configService: ConfigService) {
     return {
       ...configService.get('database'),
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       migrations: [__dirname + '/migrations/*{.ts,.js'],
     };
