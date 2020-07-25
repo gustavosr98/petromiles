10fea1ac547b67cf0a5e28891db3435e73661621
Gustavo Sanchez <gustavosr98@gmail.com>
2020-07-25 00:09:16 -0400

    FASE3-028: Eliminar imports de mas

--- petromiles-backend/src/modules/example/example.service.ts
+++ petromiles-backend/src/modules/example/example.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { getConnection } from 'typeorm';
-import { ClientBankAccount } from '../../entities/client-bank-account.entity';
-import { BankAccount } from '../../entities/bank-account.entity';
 
 @Injectable()
 export class ExampleService {
