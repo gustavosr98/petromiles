c5cc49d501b3b74f012286bcb24205f81dfdfb82
Gustavo Sanchez <gustavosr98@gmail.com>
2020-07-25 00:02:31 -0400

    FASE3-028: Ajustar example service

--- petromiles-backend/src/modules/example/example.service.ts
+++ petromiles-backend/src/modules/example/example.service.ts
@@ -5,31 +5,7 @@ import { BankAccount } from '../../entities/bank-account.entity';
 
 @Injectable()
 export class ExampleService {
-  async example() {
-    const accounts = await getConnection()
-      .getRepository(ClientBankAccount)
-      .find({
-        join: {
-          alias: 'clientBankAccount',
-          innerJoinAndSelect: {
-            bankAccount: 'clientBankAccount.bankAccount',
-          },
-        },
-      });
-    console.log('Direct relationship');
-    console.log(accounts);
-
-    const clientBankAccount = await getConnection()
-      .getRepository(BankAccount)
-      .find({
-        join: {
-          alias: 'bankAccount',
-          innerJoinAndSelect: {
-            clientBankAccount: 'bankAccount.clientBankAccount',
-          },
-        },
-      });
-    console.log('Inverse relationship');
-    console.log(clientBankAccount);
+  test(): string {
+    return 'I am working!';
   }
 }
