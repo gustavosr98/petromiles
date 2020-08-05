bfbbb890a4c8b80c4a55fd52dd745152a3f4feaa
michellealleyne <maalleyne.17@est.ucab.edu.ve>
2020-08-05 11:46:28 -0400

    FASE3-033: Crea vista para cerrar cuenta de usuario

---
+++ petromiles-frontend/src/components/Users/AccountManagement.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <br />
+    <v-divider></v-divider>
+    <br />
+    <h3 class="text-center">Account Management</h3>
+    <p class="text-center">Cierra tu cuenta de usuario permanentemente.</p>
+
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col cols="11" md="9" class="ml-4 mb-8">
+        <p class="mb-0 pb-0">Cierra tu cuenta</p>
+        <p class="mt-0 pt-0">
+          <strong>Advertencia:</strong> Si cierras tu cuenta, se cancelará tu suscripción en PetroMiles y perderás el acceso para siempre.
+        </p>
+        <v-checkbox v-model="deleteUserData" :label="checkBoxMessage"></v-checkbox>
+        <v-btn outlined color="indigo">Cerrar cuenta</v-btn>
+      </v-col>
+
+      <v-spacer></v-spacer>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      deleteUserData: false,
+      checkBoxMessage:
+        "I want to close my account and delete all my information",
+    };
+  },
+};
+</script>
