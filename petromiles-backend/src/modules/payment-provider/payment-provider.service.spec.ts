6783a3beb6c45703c632e7d3ea1097cdf78b9f74
Javier Andrade <javierandrade2107@gmail.com>
2020-08-05 17:47:56 -0400

    FASE3-020: Inicio de pruebas unitarias a PaymentProviderService

--- petromiles-backend/src/modules/payment-provider/payment-provider.service.spec.ts
+++ petromiles-backend/src/modules/payment-provider/payment-provider.service.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { PaymentProviderService } from './payment-provider.service';
+import { StripeService } from '@/modules/payment-provider/stripe/stripe.service';
+
+import { WinstonModule } from 'nest-winston';
+import createOptions from '../../logger/winston/winston-config';
 
 describe('PaymentProviderService', () => {
-  let service: PaymentProviderService;
+  let paymentProviderService: PaymentProviderService;
+  let StripeServiceMock: jest.Mock<Partial<StripeService>>;
+  let stripeService: StripeService;
+
+  beforeEach(() => {
+    StripeServiceMock = jest.fn<Partial<StripeService>, StripeService[]>(
+      () => ({
+        createBankAccountByToken: jest.fn(),
+        asociateBankAccountToAccount: jest.fn(),
+        verifyBankAccount: jest.fn(),
+        createCharge: jest.fn(),
+        createPayout: jest.fn(),
+        createAccount: jest.fn(),
+        updateBankAccountOfAnAccount: jest.fn(),
+        deleteBankAccount: jest.fn(),
+        createTransfer: jest.fn(),
+        asociateBankToCustomer: jest.fn(),
+        findAllCustomers: jest.fn(),
+        findCustomer: jest.fn(),
+        createCustomer: jest.fn(),
+        updateCustomer: jest.fn(),
+        deleteCustomer: jest.fn(),
+      }),
+    );
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentProviderService],
+      imports: [
+        WinstonModule.forRoot(
+          createOptions({ fileName: 'petromiles-global.log' }),
+        ),
+      ],
+      providers: [
+        PaymentProviderService,
+        {
+          provide: StripeService,
+          useClass: StripeServiceMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<PaymentProviderService>(PaymentProviderService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    paymentProviderService = module.get<PaymentProviderService>(
+      PaymentProviderService,
+    );
+    stripeService = module.get<StripeService>(StripeService);
   });
 });
