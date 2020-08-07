781bfae98fce9db0f9f3682b9c223063557891a5
michellealleyne <maalleyne.17@est.ucab.edu.ve>
2020-08-07 13:34:39 -0400

    FASE3-025: Crea prueba n2n para la vista de compra de puntos

---
+++ petromiles-frontend/cypress/integration/payments/buy-points.spec.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress" />
+
+context("Buy points", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get(".email-input").type("test@petromiles.com");
+    cy.get(".password-input").type("test1234");
+    cy.get(".login-btn").click();
+  });
+
+  it("trying to put letters into points field", () => {
+    cy.get(".buy-points-btn").click();
+    cy.get(".points-input").type("letters");
+    cy.get(".accounts-selector").click();
+    cy.contains("XXXX").click();
+    cy.get(".submit-btn").click();
+    cy.url().should("include", "/buy-points");
+    cy.get(".v-messages__message").should("be.visible");
+  });
+
+  it("trying to not to choose a bank account ", () => {
+    cy.get(".buy-points-btn").click();
+    cy.get(".points-input").type("500");
+    cy.get(".submit-btn").click();
+    cy.url().should("include", "/buy-points");
+    cy.get(".v-messages__message").should("be.visible");
+  });
+
+  it("trying to buy an amount in points equivalent or greater than $2000", () => {
+    cy.get(".buy-points-btn").click();
+    cy.get(".points-input").type("5000000");
+    cy.get(".accounts-selector").click();
+    cy.contains("XXXX").click();
+    cy.get(".submit-btn").click();
+    cy.get(".confirm-btn").click();
+    cy.wait(5000);
+    cy.url().should("include", "/buy-points");
+    cy.get(".error-modal").should("exist");
+  });
+
+  it("when everything goes well", () => {
+    cy.get(".buy-points-btn").click();
+    cy.get(".points-input").type("5000");
+    cy.get(".accounts-selector").click();
+    cy.contains("XXXX").click();
+    cy.get(".submit-btn").click();
+    cy.get(".confirm-btn").click();
+    cy.wait(5000);
+    cy.get(".ok-btn").click();
+    cy.url().should("include", "/transactions");
+  });
+});
