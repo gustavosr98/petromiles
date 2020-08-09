c8f2dcc990fe2cc014e3dedd29857c3a1302f838
GabTovarUCAB <gatovar.14@est.ucab.edu.ve>
2020-08-09 19:47:24 -0400

    FASE3-042: arreglando detalle en el tiempo de las pruebas

--- petromiles-frontend/cypress/integration/clients/register-user.spec.js
+++ petromiles-frontend/cypress/integration/clients/register-user.spec.js
@@ -2,94 +2,93 @@
 
 context("Buy points", () => {
     beforeEach(() => {
-      cy.visit("/");
-      cy.get(".link-to-signup").click();
+        cy.visit("/");
+        cy.get(".link-to-signup").click();
     });
-  
+
     it("trying to let all the fields empty", () => {
-      cy.get(".submit-btn").click();
-      cy.url().should("include", "/sign-up");
-      cy.get(".v-messages__message").should("be.visible");
+        cy.url().should("include", "/sign-up");
+        cy.get(".submit-btn").click();
+        cy.get(".v-messages__message").should("be.visible");
     });
-  
+
     it("trying to let the Name field empty", () => {
-      cy.get(".last-name-input").type("Doe");
-      cy.get(".email-input").type("test@petromiles.com");
-      cy.get(".password-input").type("test1234");
-      cy.get(".submit-btn").click();
-      cy.url().should("include", "/sign-up");
-      cy.get(".v-messages__message").should("be.visible");
+        cy.url().should("include", "/sign-up");
+        cy.get(".last-name-input").type("Doe");
+        cy.get(".email-input").type("test@petromiles.com");
+        cy.get(".password-input").type("test1234");
+        cy.get(".submit-btn").click();
+        cy.get(".v-messages__message").should("be.visible");
     });
-  
-    
+
+
     it("trying to let the Last-Name field empty", () => {
+        cy.url().should("include", "/sign-up");
         cy.get(".name-input").type("John");
         cy.get(".email-input").type("test@petromiles.com");
         cy.get(".password-input").type("test1234");
         cy.get(".submit-btn").click();
-        cy.url().should("include", "/sign-up");
         cy.get(".v-messages__message").should("be.visible");
-      });
+    });
 
-      it("trying to let the Password field empty", () => {
-        cy.get(".name-input").type("John");          
+    it("trying to let the Password field empty", () => {
+        cy.url().should("include", "/sign-up");
+        cy.get(".name-input").type("John");
         cy.get(".last-name-input").type("Doe");
         cy.get(".email-input").type("test@petromiles.com");
         cy.get(".submit-btn").click();
-        cy.url().should("include", "/sign-up");
         cy.get(".v-messages__message").should("be.visible");
-      });
+    });
 
-      it("trying to let the Email field empty", () => {
-        cy.get(".name-input").type("John");          
+    it("trying to let the Email field empty", () => {
+        cy.url().should("include", "/sign-up");
+        cy.get(".name-input").type("John");
         cy.get(".last-name-input").type("Doe");
         cy.get(".password-input").type("test1234");
         cy.get(".submit-btn").click();
-        cy.url().should("include", "/sign-up");
         cy.get(".v-messages__message").should("be.visible");
-      });
+    });
 
-      it("trying to input an invalid Email", () => {
-        cy.get(".name-input").type("John");          
+    it("trying to input an invalid Email", () => {
+        cy.url().should("include", "/sign-up");
+        cy.get(".name-input").type("John");
         cy.get(".last-name-input").type("Doe");
         cy.get(".email-input").type("test@petromiles");
         cy.get(".password-input").type("test1234");
         cy.get(".submit-btn").click();
-        cy.url().should("include", "/sign-up");
         cy.get(".v-messages__message").should("be.visible");
-      });
+    });
 
-      it("trying to input an invalid Password", () => {
-        cy.get(".name-input").type("John");          
+    it("trying to input an invalid Password", () => {
+        cy.url().should("include", "/sign-up");
+        cy.get(".name-input").type("John");
         cy.get(".last-name-input").type("Doe");
         cy.get(".email-input").type("test@petromiles.com");
         cy.get(".password-input").type("test1");
         cy.get(".submit-btn").click();
-        cy.url().should("include", "/sign-up");
         cy.get(".v-messages__message").should("be.visible");
-      });
+    });
 
-      it("trying register a new user correctly", () => {
-        cy.url().should("include", "/sign-up");          
-        cy.get(".name-input").type("John");          
+    it("trying register a new user correctly", () => {
+        cy.url().should("include", "/sign-up");
+        cy.get(".name-input").type("John");
         cy.get(".last-name-input").type("Doe");
         cy.get(".email-input").type("test@petromiles.com");
         cy.get(".password-input").type("test1234");
         cy.get(".submit-btn").click();
         cy.wait(12000);
         cy.url().should("include", "/dashboard");
-      });
-  
-      it("trying to register a new user with a email already in use", () => {
-        cy.url().should("include", "/sign-up");          
-        cy.get(".name-input").type("John");          
+    });
+
+    it("trying to register a new user with a email already in use", () => {
+        cy.url().should("include", "/sign-up");
+        cy.get(".name-input").type("John");
         cy.get(".last-name-input").type("Doe");
         cy.get(".email-input").type("test@petromiles.com");
         cy.get(".password-input").type("test1234");
         cy.get(".submit-btn").click();
         cy.wait(12000);
         cy.get(".ok-btn").click();
-      });
-  
-  });
-  
+    });
+
+});
