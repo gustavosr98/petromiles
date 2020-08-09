147c59ffb24d58690a3875c923c320603cdef767
Javier Andrade <javierandrade2107@gmail.com>
2020-08-09 03:48:52 -0400

    FASE3-040: Configuración de jest en front, solución de errores

--- petromiles-frontend/jest.config.js
+++ petromiles-frontend/jest.config.js
@@ -1,4 +1,19 @@
+const { defaults } = require("jest-config");
+
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  testMatch: ["**/test/**/*.spec.js", "**/src/**/*.spec.js"],
+  ...defaults,
+  testMatch: ["**/test/**/*.spec.js"],
+  rootDir: ".",
+  moduleNameMapper: {
+    "@/(.*)$": "<rootDir>/src/$1",
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "^.+\\.(js|jsx)?$": "babel-jest",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/$1"],
+  transformIgnorePatterns: ["<rootDir>/node_modules/$1"],
 };
--- petromiles-frontend/test/unit-tests/exchange-points.spec.js
+++ petromiles-frontend/test/unit-tests/exchange-points.spec.js
@@ -6,6 +6,10 @@ import router from "@/router";
 import store from "@/store";
 import vuetify from "@/plugins/vuetify";
 import i18n from "@/i18n";
+import httpClient from "@/http/http-client";
+
+Vue.config.productionTip = false;
+Vue.prototype.$http = httpClient;
 
 const wrapper = shallowMount(ExchangePointsForm, {
   router,
