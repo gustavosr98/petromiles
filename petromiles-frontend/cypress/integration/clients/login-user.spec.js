e67c8c2e2e7d836d692cb8b1f2c74b50ae2ee895
GabTovarUCAB <gatovar.14@est.ucab.edu.ve>
2020-08-10 02:34:08 -0400

    FASE3-044: dejando el campo de correo en blanco

--- petromiles-frontend/cypress/integration/clients/login-user.spec.js
+++ petromiles-frontend/cypress/integration/clients/login-user.spec.js
@@ -7,61 +7,23 @@ context("Buy points", () => {
   
     // it("trying to let all the fields empty", () => {
     //     cy.get(".login-btn").click();
+        //    cy.wait(3000);
     //     cy.get(".ok-btn").click();
     // });
     
-    it("trying to let the password field empty", () => {
-        cy.get(".email-input").type("test@petromiles.com");
+    // it("trying to let the password field empty", () => {
+    //     cy.get(".email-input").type("test@petromiles.com");
+    //     cy.get(".login-btn").click();
+    //     cy.wait(3000);
+    //     cy.get(".ok-btn").click();
+    // });
+
+    it("trying to let the email field empty", () => {
+        cy.get(".password-input").type("test1234");
         cy.get(".login-btn").click();
+        cy.wait(3000);
         cy.get(".ok-btn").click();
     });
-  
-    // it("trying to let the points field empty", () => {
-    //   cy.get(".accounts-selector").click();
-    //   cy.contains("XXXX").click();
-    //   cy.get(".submit-btn").click();
-    //   cy.url().should("include", "/buy-points");
-    //   cy.get(".v-messages__message").should("be.visible");
-    // });
-  
-    // it("trying to put letters into points field", () => {
-    //   cy.get(".points-input").type("letters");
-    //   cy.get(".accounts-selector").click();
-    //   cy.contains("XXXX").click();
-    //   cy.get(".submit-btn").click();
-    //   cy.url().should("include", "/buy-points");
-    //   cy.get(".v-messages__message").should("be.visible");
-    // });
-  
-    // it("trying to not to choose a bank account ", () => {
-    //   cy.get(".points-input").type("500");
-    //   cy.get(".submit-btn").click();
-    //   cy.url().should("include", "/buy-points");
-    //   cy.get(".v-messages__message").should("be.visible");
-    // });
-  
-    // it("trying to buy an amount in points equivalent or greater than $2000", () => {
-    //   cy.get(".points-input").type("5000000");
-    //   cy.get(".accounts-selector").click();
-    //   cy.contains("XXXX").click();
-    //   cy.get(".submit-btn").click();
-    //   cy.get(".confirm-btn").click();
-    //   cy.wait(5000);
-    //   cy.url().should("include", "/buy-points");
-    //   cy.contains("Charge amount exceeds bank account limit").should(
-    //     "be.visible"
-    //   );
-    // });
-  
-    // it("when everything goes well", () => {
-    //   cy.get(".points-input").type("500000");
-    //   cy.get(".accounts-selector").click();
-    //   cy.contains("XXXX").click();
-    //   cy.get(".submit-btn").click();
-    //   cy.get(".confirm-btn").click();
-    //   cy.wait(5000);
-    //   cy.get(".ok-btn").click();
-    //   cy.url().should("include", "/transactions");
-    // });
+
   });
   
