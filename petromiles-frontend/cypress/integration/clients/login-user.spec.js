488690e15e7eb808013f95b95503df6a96479672
GabTovarUCAB <gatovar.14@est.ucab.edu.ve>
2020-08-10 02:47:43 -0400

    FASE3-044: cuando ingresa los datos correctamente

--- petromiles-frontend/cypress/integration/clients/login-user.spec.js
+++ petromiles-frontend/cypress/integration/clients/login-user.spec.js
@@ -5,33 +5,33 @@ context("Buy points", () => {
       cy.visit("/");
     });
   
-    // it("trying to let all the fields empty", () => {
-    //     cy.get(".login-btn").click();
-        //    cy.wait(3000);
-    //     cy.get(".ok-btn").click();
-    // });
+    it("trying to let all the fields empty", () => {
+        cy.get(".login-btn").click();
+           cy.wait(3000);
+        cy.get(".ok-btn").click();
+    });
     
-    // it("trying to let the password field empty", () => {
-    //     cy.get(".email-input").type("test@petromiles.com");
-    //     cy.get(".login-btn").click();
-    //     cy.wait(3000);
-    //     cy.get(".ok-btn").click();
-    // });
+    it("trying to let the password field empty", () => {
+        cy.get(".email-input").type("test@petromiles.com");
+        cy.get(".login-btn").click();
+        cy.wait(3000);
+        cy.get(".ok-btn").click();
+    });
 
-    // it("trying to let the email field empty", () => {
-    //     cy.get(".password-input").type("test1234");
-    //     cy.get(".login-btn").click();
-    //     cy.wait(3000);
-    //     cy.get(".ok-btn").click();
-    // });
+    it("trying to let the email field empty", () => {
+        cy.get(".password-input").type("test1234");
+        cy.get(".login-btn").click();
+        cy.wait(3000);
+        cy.get(".ok-btn").click();
+    });
 
-    // it("trying to login with a wrong email", () => {
-    //     cy.get(".email-input").type("test182@petromiles.com");
-    //     cy.get(".password-input").type("test1234");
-    //     cy.get(".login-btn").click();
-    //     cy.wait(3000);
-    //     cy.get(".ok-btn").click();
-    // });
+    it("trying to login with a wrong email", () => {
+        cy.get(".email-input").type("test182@petromiles.com");
+        cy.get(".password-input").type("test1234");
+        cy.get(".login-btn").click();
+        cy.wait(3000);
+        cy.get(".ok-btn").click();
+    });
     
     it("trying to login with a wrong password", () => {
         cy.get(".email-input").type("test@petromiles.com");
@@ -40,6 +40,13 @@ context("Buy points", () => {
         cy.wait(3000);
         cy.get(".ok-btn").click();
     });
-
+    
+    it("trying to login succesfuly", () => {
+        cy.get(".email-input").type("test@petromiles.com");
+        cy.get(".password-input").type("test1234");
+        cy.get(".login-btn").click();
+        cy.wait(5000);
+        cy.url().should("include", "/dashboard");
+    });
   });
   
