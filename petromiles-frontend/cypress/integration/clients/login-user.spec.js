0a91e85918b880cc7e04493cfc44c6a9d3ecff20
GabTovarUCAB <gatovar.14@est.ucab.edu.ve>
2020-08-10 02:42:43 -0400

    FASE3-044: cuando intenta acceder con una contraseña equivocada

--- petromiles-frontend/cypress/integration/clients/login-user.spec.js
+++ petromiles-frontend/cypress/integration/clients/login-user.spec.js
@@ -25,14 +25,21 @@ context("Buy points", () => {
     //     cy.get(".ok-btn").click();
     // });
 
-    it("trying to login with a wrong email", () => {
-        cy.get(".email-input").type("test182@petromiles.com");
-        cy.get(".password-input").type("test1234");
+    // it("trying to login with a wrong email", () => {
+    //     cy.get(".email-input").type("test182@petromiles.com");
+    //     cy.get(".password-input").type("test1234");
+    //     cy.get(".login-btn").click();
+    //     cy.wait(3000);
+    //     cy.get(".ok-btn").click();
+    // });
+    
+    it("trying to login with a wrong password", () => {
+        cy.get(".email-input").type("test@petromiles.com");
+        cy.get(".password-input").type("wrongtest1234");
         cy.get(".login-btn").click();
         cy.wait(3000);
         cy.get(".ok-btn").click();
     });
-
 
   });
   
