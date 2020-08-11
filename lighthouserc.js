04b98e1b7f46e65928fda81df678c309e23d9b72
Gustavo Sanchez <gustavosr98@gmail.com>
2020-08-11 06:43:25 -0400

    FASE3-028: Lighthouse test

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ["http://localhost:5000/"],
+      startServerCommand: "npm run start",
+    },
     upload: {
       target: "temporary-public-storage",
     },
