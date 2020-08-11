0ed1b3f3473b49f74c29cca1f83c8c6f5095329d
mecoccaro <mecoccaro.16@est.ucab.edu.ve>
2020-08-11 01:21:04 -0400

    FASE3-041: Cambio del identify al navbar

--- petromiles-frontend/src/main.js
+++ petromiles-frontend/src/main.js
@@ -19,27 +19,6 @@ import "firebase/auth";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 import LogRocket from 'logrocket';
-import authConstants from "./constants/authConstants";
-
-
-
-const user = JSON.parse(
-    localStorage.getItem(authConstants.USER_LOCAL_STORAGE)
-);
-
-if (user !== null){
-  const userId = toString(user.details.idUserDetails);
-  const userName = user.details.firstName + ' ' + user.details.lastName;
-  const userEmail = user.email;
-
-  LogRocket.identify(userId, {
-    name: userName,
-    email: userEmail,
-  });
-}
-
-
-
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = httpClient;
