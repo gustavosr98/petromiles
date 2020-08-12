7b2cf570ab9e2475c480c4b9054dd54fe4750061
RafaelMendezUCAB <rrmendez.17@est.ucab.edu.ve>
2020-08-11 21:03:50 -0400

    FASE3-053: Implementadas pruebas para componente ExchangePointsForm

--- petromiles-frontend/tests/unit/exchangePointsForm.spec.js
+++ petromiles-frontend/tests/unit/exchangePointsForm.spec.js
@@ -24,4 +24,58 @@ describe("Testing ExchangePointsForm component", () => {
         vuetify,
     })  
 
+    it("Assigns 150 points to exchange", () => {
+        wrapper.setData({ points: 150 });
+        expect(wrapper.vm.points).toBe(150);
+    })
+
+    it("Calculates rawCost", () => {
+        const data = {
+            points: 500,
+            onePointToDollars: 2
+        }        
+        const calc = ExchangePointsForm.computed.rawCost.call(data);
+        expect(calc).toBe(1000);
+    })
+
+    it("Calculates cost with interests", () => {
+        const data = {
+            points: 500,
+            rawCost: 0,
+            onePointToDollars: 2,
+            interests: [
+                {
+                    operation: 1,
+                    amount: 75,
+                    percentage: 0
+                },
+                {
+                    operation: 1,
+                    amount: 0,
+                    percentage: 0.05
+                }
+            ]
+        }
+        data.rawCost = ExchangePointsForm.computed.rawCost.call(data);        
+        const costWithInterests = ExchangePointsForm.computed.costWithInterests.call(data);        
+        expect(costWithInterests).toBe(949.25);
+    })
+
+    it("Calculates total points left after exchanged", () => {
+        const data = {
+            points:150,
+            totalPoints: 2000
+        }
+        const pointsLeft = ExchangePointsForm.computed.totalPointsRest.call(data);
+        expect(pointsLeft).toBe(1850);
+    })
+
+    it("Executes exchangePoints method correctly", async () => {
+        const exchangePoints = jest.fn();
+        wrapper.setMethods({ exchangePoints: exchangePoints });
+        wrapper.find("v-btn.confirm-btn").trigger("click");
+        await wrapper.vm.$nextTick()
+        expect(exchangePoints).toHaveBeenCalled();        
+    })
+
 })
